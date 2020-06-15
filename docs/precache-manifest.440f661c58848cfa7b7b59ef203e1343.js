e2edf1ac492b74853fd7e30c31d8a0de9b474e0e
魏一鸣 <sssqbslyl@live.com>
2020-06-15 14:39:00 +0800

    add build files

---
+++ docs/precache-manifest.440f661c58848cfa7b7b59ef203e1343.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad2683abeff01728f3e9c684bf2825c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023a6a5122d9dc9614b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f68f4fbc2de194a1b560",
+    "url": "/static/js/2.b60ca17a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b60ca17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023a6a5122d9dc9614b8",
+    "url": "/static/js/main.4a7c381d.chunk.js"
+  },
+  {
+    "revision": "0beba14462c6fcd9d6ed",
+    "url": "/static/js/runtime-main.202dff18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
