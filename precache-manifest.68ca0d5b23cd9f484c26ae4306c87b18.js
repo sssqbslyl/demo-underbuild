ae974c2233e96c336872f74d7c3833046fc96851
魏一鸣 <sssqbslyl@live.com>
2020-06-15 15:04:40 +0800

    Updates

---
+++ precache-manifest.68ca0d5b23cd9f484c26ae4306c87b18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c279769aac5e41bcdeced50404b62b0b",
+    "url": "/demo-underbuild/index.html"
+  },
+  {
+    "revision": "aedfc9baf0613e518b28",
+    "url": "/demo-underbuild/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f68f4fbc2de194a1b560",
+    "url": "/demo-underbuild/static/js/2.b60ca17a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-underbuild/static/js/2.b60ca17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedfc9baf0613e518b28",
+    "url": "/demo-underbuild/static/js/main.aae89aaf.chunk.js"
+  },
+  {
+    "revision": "6d467166ab668fa6bea5",
+    "url": "/demo-underbuild/static/js/runtime-main.b7ff9f01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-underbuild/static/media/logo.5d5d9eef.svg"
+  }
+]);
